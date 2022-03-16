782cdacb789a2eedaf2d105ba60dcff88ce9ee7e
邴新科 <kylebing@163.com>
2022-03-16 10:44:57 +0800

    publish

--- class/config.php
+++ class/config.php
@@ -4,4 +4,4 @@ define('PORT',          '3306');            // 数据库端口
 define('DATABASE',      'diary');           // 数据库名
 define('USER',          '----');            // 数据库用户名
 define('PASSWORD',      '----');            // 数据库密码
-define('INVITATION',    'kylebingooOO');    // 邀请码，注册用户时使用，
+define('INVITATION',    '----');            // 邀请码，注册用户时使用，
