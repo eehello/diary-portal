4d0bea92e0b592af5771267960ce8abd53f2668d
邴新科 <kylebing@163.com>
2022-03-16 11:15:09 +0800

    update diary.sql

--- diary.sql
+++ diary.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : kylebing.cn
+ Source Server         : 512
  Source Server Type    : MariaDB
  Source Server Version : 50568
  Source Host           : kylebing.cn:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50568
  File Encoding         : 65001
 
- Date: 08/05/2021 09:13:48
+ Date: 16/03/2022 11:14:31
 */
 
 SET NAMES utf8mb4;
@@ -21,35 +21,35 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for diaries
 -- ----------------------------
 DROP TABLE IF EXISTS `diaries`;
-CREATE TABLE `diaries` (
+CREATE TABLE `diaries`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL COMMENT '日记日期',
-  `title` text NOT NULL COMMENT '标题',
-  `content` longtext COMMENT '内容',
-  `temperature` int(3) DEFAULT '-273' COMMENT '室内温度',
-  `temperature_outside` int(3) DEFAULT '-273' COMMENT '室外温度',
-  `weather` enum('sunny','cloudy','overcast','sprinkle','rain','thunderstorm','fog','snow','tornado','smog','sandstorm') NOT NULL DEFAULT 'sunny' COMMENT '天气',
-  `category` enum('life','study','film','game','work','sport','bigevent','week','article') NOT NULL DEFAULT 'life' COMMENT '类别',
-  `date_create` datetime NOT NULL COMMENT '创建日期',
-  `date_modify` datetime DEFAULT NULL COMMENT '编辑日期',
+  `date` datetime(0) NOT NULL COMMENT '日记日期',
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题',
+  `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '内容',
+  `temperature` int(3) NULL DEFAULT -273 COMMENT '室内温度',
+  `temperature_outside` int(3) NULL DEFAULT -273 COMMENT '室外温度',
+  `weather` enum('sunny','cloudy','overcast','sprinkle','rain','thunderstorm','fog','snow','tornado','smog','sandstorm') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'sunny' COMMENT '天气',
+  `category` enum('life','study','film','game','work','sport','bigevent','week','article') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'life' COMMENT '类别',
+  `date_create` datetime(0) NOT NULL COMMENT '创建日期',
+  `date_modify` datetime(0) NULL DEFAULT NULL COMMENT '编辑日期',
   `uid` int(11) NOT NULL COMMENT '用户id',
-  `is_public` int(1) NOT NULL DEFAULT '0' COMMENT '是否共享',
+  `is_public` int(1) NOT NULL DEFAULT 0 COMMENT '是否共享',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1805 DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB AUTO_INCREMENT = 5344 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE `users`  (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `last_visit_time` datetime DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `register_time` datetime DEFAULT NULL,
-  `comment` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`uid`,`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_visit_time` datetime(0) NULL DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `register_time` datetime(0) NULL DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`uid`, `email`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 37 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
